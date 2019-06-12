ab9b98656b96451fa42332e95afc00c395b8059a
hema <Hvuser@EBHVR04-10>
2019-06-12 12:22:57 +0530

    Added WorkoutModel

---
+++ WT_UserInterface/ViewModels/WorkoutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace WT_UserInterface.ViewModels
+{
+    public class WorkoutViewModel
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Name")]
+        public string Name { get; set; }
+        [StringLength(50,ErrorMessage ="Title cannot be more than 50")]
+        [Display(Name ="Workout Title")]
+        [Required]
+        public string Workout_title { get; set; }
+        [Required]
+        [Display(Name ="Category")]
+        public string Workout_category { get; set; }
+       
+    }
+}
