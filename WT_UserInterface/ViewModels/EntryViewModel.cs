bf774cf932328d32cb15b58ed870472632e27b5c
Arun <Hvuser@DESKTOP-HQOQL3I>
2019-06-12 12:05:56 +0530

    Classes Added

---
+++ WT_UserInterface/ViewModels/EntryViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace DataAccessLayer
+{
+    /// <summary>
+    /// Represents workout entry in the workout table
+    /// Second line in comment
+    /// </summary>
+    public class EntriesViewModel
+    {
+        public int EntryNo { get; set; }
+        public int Workout_id { get; set; }
+
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yyyy}")]
+        [Display(Name = "Start Date")]
+        public DateTime start_date { get; set; }
+
+
+        [Required]
+        [Display(Name = "Start Time")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm:ss}")]
+        public DateTime start_time { get; set; }
+
+        [Display(Name = "End Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yyyy}")]
+        public DateTime end_date { get; set; }
+        
+        [Display(Name = "End Time")]
+        [DisplayFormat(DataFormatString = "{0:hh:mm:ss}")]
+        public DateTime end_time { get; set; }
+
+
+        public string status { get; set; }
+
+    }
+}
